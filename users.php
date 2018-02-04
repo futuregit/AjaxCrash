342c4b7953efe985100197ff5f267ea9b5b6726e
Tman <tomcis102@hotmail.com.noreply.github.com>
2018-02-04 16:25:12 -0500

    Initial commit

---
+++ users.php
@@ -0,0 +1,12 @@
+<?php
+include 'db.php';
+
+$query = 'SELECT * FROM users';
+
+// Get Result
+$result = mysqli_query($conn, $query);
+
+// Fetch Data
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo json_encode($users);
